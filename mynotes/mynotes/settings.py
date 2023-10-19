f1aca5e15d4867e773ea183dca49b5c993605acd
prashant2231109 <prashant2231109@akgec.ac.in>
2023-10-19 10:41:23 +0530

    added host

--- mynotes/mynotes/settings.py
+++ mynotes/mynotes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fom99ku-33#zsy=4)9458aek*73+dfuj#d+dlzj&d&jp$xyv(s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
